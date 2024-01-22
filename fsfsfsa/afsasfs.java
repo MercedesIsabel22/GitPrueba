f7e571300bb6b6a8aa9e2b2d83778dce78d33ada
MercedesIsabel22 <Mercedesnaba22@gmail.com>
2024-01-21 22:43:19 -0500

    probandoGit

---
+++ fsfsfsa/afsasfs.java
@@ -0,0 +1,43 @@
+package fsfsfsa;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class afsasfs {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					afsasfs window = new afsasfs();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public afsasfs() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
